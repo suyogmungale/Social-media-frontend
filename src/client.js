e0ee3bb81604177dc300bfa6bc236f77fb7d082a
suyogmungale <suyogmungale5@gmail.com>
2023-03-17 22:11:20 +0530

    initial

--- src/client.js
+++ src/client.js
@@ -1,11 +1,18 @@
 import { createClient } from '@sanity/client';
 
+const projectid = process.env.REACT_APP_SANITY_PROJECT_ID;
+const token = process.env.REACT_APP_SANITY_PROJECT_TOKEN;
+
+console.log(projectid);
+console.log(token);
+
+
 const client = createClient({
-  projectId: 'n7301ijd', // Replace with your project ID
+  projectId:projectid, // Replace with your project ID
   dataset: 'production',
   apiVersion:'2021-11-16',
   useCdn: true,
-  token:'skL2896ewyhXphX95BUz5PY3NX9TKKnHiVYPb4ywMVdhOWgpAMpk5NdmhvIpZdeCsm0aB9pQWRFPn9HhjrLMQK0gR1gEWt6o4VmzMLxRHCOgvH0PiUFHnETX8RnwiA1QmxILkOdgOkrHeQO1TJyRr632360dPIlJIowH68T7zf9JNBuyvnfB',
+  token:token,
   ignoreBrowserTokenWarning: true       
 });
 
